d7716b66701ef4062e1e1b83e1489056f13d4e93
Volodymyr Maiboroda <volodymyr.maiboroda@logianalytics.com>
2020-04-05 19:58:16 +0300

    init project

---
+++ src/test/java/ua/nure/virtual/nutritionist/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nure.virtual.nutritionist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
